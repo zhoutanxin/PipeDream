f74891f7e2f388a6fd49510bfdc676ae67b08ffd
zhoutanxin <zhoutanxin@hotmail.com>
2015-03-27 12:56:53 +0800

    一个游戏开发学习型项目

---
+++ src/app.js
@@ -0,0 +1,150 @@
+
+var GameLayer = cc.Layer.extend({
+    mWinSize:null,
+    mPointCenter:null,
+    mHero: null,
+    mRobot: null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+        mWinSize = cc.winSize;
+        mPointCenter = cc.p(mWinSize.width / 2, mWinSize.height / 2);
+        // 添加背景
+        var backGround = cc.Sprite.create(res.s_BackGround);
+        backGround.setPosition(mPointCenter);
+        this.addChild(backGround);
+        // 添加控制层
+        var hudLayer = HudLayer.create();
+        this.addChild(hudLayer);
+        hudLayer.setDelegate(this);
+
+        // 添加英雄
+        this.mHero = Hero.create();
+        // 添加机器人
+        this.mRobot = Robot.create();
+//        this.addChild(this.mRobot);
+        var characters = cc.Node.create();
+        characters.addChild(this.mHero);
+        characters.addChild(this.mRobot);
+        this.addChild(characters);
+
+        // 骨骼动画测试
+//        this.test();
+//
+//        bRef = true;
+        // add a "close" icon to exit the progress. it's an autorelease object
+//        var closeItem = new cc.MenuItemImage(
+//            res.CloseNormal_png,
+//            res.CloseSelected_png,
+//            function () {
+//                cc.log("Menu is clicked!");
+//            }, this);
+//        closeItem.attr({
+//            x: size.width - 20,
+//            y: 20,
+//            anchorX: 0.5,
+//            anchorY: 0.5
+//        });
+//
+//        var menu = new cc.Menu(closeItem);
+//        menu.x = 0;
+//        menu.y = 0;
+//        this.addChild(menu, 1);
+
+        /////////////////////////////
+        // 3. add your codes below...
+        // add a label shows "Hello World"
+        // create and initialize a label
+        // position the label on the center of the screen
+//        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
+//        helloLabel.x = size.width / 2;
+//        helloLabel.y = 0;
+//        this.addChild(helloLabel, 5);
+        // add the label as a child to this layer
+
+
+        // add "HelloWorld" splash screen"
+//        this.sprite = new cc.Sprite(res.HelloWorld_png);
+//        this.sprite.attr({
+//            x: size.width / 2,
+//            y: size.height / 2,
+//            scale: 0.5,
+//            rotation: 180
+//        });
+//        this.addChild(this.sprite, 0);
+//
+//        this.sprite.runAction(
+//            cc.sequence(
+//                cc.rotateTo(2, 0),
+//                cc.scaleTo(2, 1, 1)
+//            )
+//        );
+//        helloLabel.runAction(
+//            cc.spawn(
+//                cc.moveBy(2.5, cc.p(0, size.height - 40)),
+//                cc.tintTo(2.5,255,125,0)
+//            )
+//        );
+        return true;
+    },
+
+    actionJoypadStart:function(degrees){
+        this.mHero.runWithDegrees(degrees);
+        this.mRobot.runWithDegrees(degrees);
+    },
+    actionJoypadUpdate:function(degrees){
+        this.mHero.moveWithDegrees(degrees);
+        this.mRobot.moveWithDegrees(degrees);
+    },
+    actionJoypadEnded:function(degrees){
+        this.mHero.idle();
+        this.mRobot.idle();
+    },
+    attackButtonClick:function(button){
+        this.mHero.attack(button);
+        this.mRobot.attack(button);
+    },
+    onKeyDown:function(e){
+        // 保存所有的按键信息
+        AC.KEYS[e] = true;
+    },
+    onKeyUp:function(e){
+        AC.KEYS[e] = false;
+    },
+    update:function(dt){
+        var b = this.mHero.getZLocation() > this.mRobot.getZLocation();
+        this.mHero.setZOrder(b ? 0: 1);
+        this.mRobot.setZOrder(b ? 1: 0) ;
+    },
+    test:function(){
+        return;
+        ccs.armatureDataManager.getInstance().addArmatureFileInfo(
+            s_Robot_png,
+            s_Robot_plist,
+            s_Robot_json);
+
+        var armature = cc.Armature.create("Animation1");
+        armature.getAnimation().PlayByIndex(0);
+        armature.setAnchorPoint(cc.p(0.5, 0.5));
+        armature.setPosition(cc.p(300, 300));
+        this.addChild(armature);
+        cc.log("armature add ");
+
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new GameLayer();
+        this.addChild(layer);
+    }
+});
+
---
+++ src/resource.js
@@ -0,0 +1,92 @@
+var res = {
+    s_HelloWorld : "res/HelloWorld.png",
+    s_CloseNormal : "res/CloseNormal.png",
+    s_CloseSelected : "res/CloseSelected.png",
+    s_BackGround : "res/BackGround.png",
+ s_Attack : "res/Attack.png",
+ s_AttackO : "res/AttackO.png",
+ s_AttackA : "res/AttackA.png",
+ s_AttackB : "res/AttackB.png",
+ s_AttackFreeze : "res/AttackFreeze.png",
+
+ s_Joypad1 : "res/Joypad1.png",
+ s_Joypad2 : "res/Joypad2.png",
+ s_Joypad3 : "res/Joypad3.png",
+ s_Joypad4 : "res/Joypad4.png",
+
+
+ s_Hero1 : "res/Hero1.png",
+ s_Hero2 : "res/Hero2.png",
+ s_Hero3 : "res/Hero3.png",
+
+ s_HeroState1 : "res/HeroState1.png",
+ s_HeroState2 : "res/HeroState2.png",
+ s_HeroState3 : "res/HeroState3.png",
+ s_HeroState4 : "res/HeroState4.png",
+
+ s_HeroRun1 : "res/HeroRun1.png",
+ s_HeroRun2 : "res/HeroRun2.png",
+ s_HeroRun3 : "res/HeroRun3.png",
+ s_HeroRun4 : "res/HeroRun4.png",
+ s_HeroRun5 : "res/HeroRun5.png",
+ s_HeroRun6 : "res/HeroRun6.png",
+ s_HeroRun7 : "res/HeroRun7.png",
+ s_HeroRun8 : "res/HeroRun8.png",
+ s_HeroRun9 : "res/HeroRun9.png",
+ s_HeroRun10 : "res/HeroRun10.png",
+ s_HeroRun11 : "res/HeroRun11.png",
+
+ s_HeroAttack1 : "res/HeroAttack1.png",
+ s_HeroAttack2 : "res/HeroAttack2.png",
+ s_HeroAttack3 : "res/HeroAttack3.png",
+ s_HeroAttack4 : "res/HeroAttack4.png",
+ s_HeroAttack5 : "res/HeroAttack5.png",
+
+ s_HeroAttackJ1 : "res/HeroAttackJ1.png",
+ s_HeroAttackJ2 : "res/HeroAttackJ2.png",
+ s_HeroAttackJ3 : "res/HeroAttackJ3.png",
+ s_HeroAttackJ4 : "res/HeroAttackJ4.png",
+ s_HeroAttackJ5 : "res/HeroAttackJ5.png",
+ s_HeroAttackJ6 : "res/HeroAttackJ6.png",
+ s_HeroAttackJ7 : "res/HeroAttackJ7.png",
+ s_HeroAttackJ8 : "res/HeroAttackJ8.png",
+
+ s_HeroAttackT1 : "res/HeroAttackT1.png",
+ s_HeroAttackT2 : "res/HeroAttackT2.png",
+ s_HeroAttackT3 : "res/HeroAttackT3.png",
+
+
+ s_Robot1 : "res/Robot1.png",
+ s_Robot2 : "res/Robot2.png",
+ s_Robot3 : "res/Robot3.png",
+
+ s_RobotState1 : "res/RobotState1.png",
+ s_RobotState2 : "res/RobotState2.png",
+ s_RobotState3 : "res/RobotState3.png",
+ s_RobotState4 : "res/RobotState4.png",
+
+
+ s_RobotAttack1 : "res/RobotAttack1.png",
+ s_RobotAttack2 : "res/RobotAttack2.png",
+ s_RobotAttack3 : "res/RobotAttack3.png",
+ s_RobotAttack4 : "res/RobotAttack4.png",
+ s_RobotAttack5 : "res/RobotAttack5.png",
+
+ s_futura48 : "res/fonts/futura-48.png",
+ s_fnt_futura48 : "res/fonts/futura-48.fnt",
+
+//  s_Robot_png : "res/armature/Robot.png",
+//  s_Robot_plist : "res/armature/Robot.plist",
+//  s_Robot_json : "res/armature/Robot.json",
+ s_Robot_png : "res/armature/NewProject0.png",
+ s_Robot_plist : "res/armature/NewProject0.plist",
+ s_Robot_json : "res/armature/NewProject.json"
+};
+
+
+
+var g_resources = [];
+for ( i in res) {
+    g_resources.push(res[i]);
+}
+
